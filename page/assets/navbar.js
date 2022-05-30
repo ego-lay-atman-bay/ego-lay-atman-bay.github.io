52d9ce9e4baece7ce4ea8e805e90e15e655ef135
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2022-05-30 16:30:29 -0600

    create page

---
+++ page/assets/navbar.js
@@ -0,0 +1,8 @@
+function topnavOpen() {
+    var x = document.getElementById("topnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
