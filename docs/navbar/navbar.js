330902a9ef48441a463c63a5dc0ac1270ebfa052
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2022-05-31 15:22:52 -0600

    remove unused function

--- docs/navbar/navbar.js
+++ docs/navbar/navbar.js
@@ -2,15 +2,6 @@ $(function(){
   $("#navbar").load("/navbar/navbar.html"); 
 });
 
-function topnavOpen() {
-    var x = document.getElementById("topnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-  }
-}
-
 // document.addEventListener('mousemove', e => {
 //   console.log(document.elementsFromPoint(e.clientX, e.clientY) )
 // }, {passive: true})
