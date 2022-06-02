1d33236eebc8f2fd59cc400445302a315eaf3f12
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2022-06-02 17:05:49 -0600

    trying method to hide dropdown

--- docs/navbar/navbar.js
+++ docs/navbar/navbar.js
@@ -1,3 +1,13 @@
-$(function(){
-  $("#navbar").load("/navbar/navbar.html"); 
-});
+document.addEventListener('mousedown', e => {
+  elements = document.elementsFromPoint(e.clientX, e.clientY)
+  console.log(elements)
+  dropdown = document.getElementsByClassName('projects')[0]
+
+  if (!elements.indexOf(dropdown) !== -1) {
+    dropdown.classList.remove('projects')
+    dropdown.classList.add('projects')
+  }
+
+  // console.log(document.querySelectorAll(':hover'))
+  // document.getElementById('debug').innerText = document.querySelectorAll(':hover')[0].outerHTML
+}, {})
