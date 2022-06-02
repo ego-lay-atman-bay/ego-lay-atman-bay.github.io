ff85f9c45e8b8936f6aa1cba998a9dd362656fb8
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2022-06-02 17:10:39 -0600

    Update navbar.js

--- docs/navbar/navbar.js
+++ docs/navbar/navbar.js
@@ -1,6 +1,5 @@
 document.addEventListener('mousedown', e => {
   elements = document.elementsFromPoint(e.clientX, e.clientY)
-  console.log(elements)
   dropdown = document.getElementsByClassName('projects')[0]
 
   if (!elements.indexOf(dropdown) !== -1) {
@@ -10,4 +9,4 @@ document.addEventListener('mousedown', e => {
 
   // console.log(document.querySelectorAll(':hover'))
   // document.getElementById('debug').innerText = document.querySelectorAll(':hover')[0].outerHTML
-}, {})
+}, {passive: true})
