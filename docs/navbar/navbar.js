52e419defe582f887a3b56f061bed9309abd5eea
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2022-06-02 14:57:38 -0600

    switch to different method

--- docs/navbar/navbar.js
+++ docs/navbar/navbar.js
@@ -15,19 +15,34 @@ function removeClass(ele,cls) {
   }
 }
 
-window.onload = function() {
-  // if (navigator.userAgent.toLowerCase().indexOf("android") > -1) {
-    var elements = document.getElementsByClassName("projects");
-    for(var i = 0; i < elements.length; i++) {
-      var elm = elements[i];
-      elm.addEventListener("touchstart", function() {
-        addClass(this, "fake-active");}, false);
-      elm.addEventListener("touchmove", function() {
-        removeClass(this, "fake-active");}, false);
-      elm.addEventListener("touchend", function() {
-        removeClass(this, "fake-active");}, false);
-      elm.addEventListener("touchcancel", function() {
-        removeClass(this, "fake-active");}, false);
-    }
-  // }
-}
+// window.onload = function() {
+//   // if (navigator.userAgent.toLowerCase().indexOf("android") > -1) {
+//     var elements = document.getElementsByClassName("projects");
+//     for(var i = 0; i < elements.length; i++) {
+//       var elm = elements[i];
+//       elm.addEventListener("touchstart", function() {
+//         addClass(this, "fake-active");}, false);
+//       elm.addEventListener("touchmove", function() {
+//         removeClass(this, "fake-active");}, false);
+//       elm.addEventListener("touchend", function() {
+//         removeClass(this, "fake-active");}, false);
+//       elm.addEventListener("touchcancel", function() {
+//         removeClass(this, "fake-active");}, false);
+//     }
+//   // }
+// }
+
+if (true) {
+  $(".projects")
+  .bind("touchstart", function () {
+      $(this).addClass("fake-active");
+  })
+  .bind("touchend", function() {
+      $(this).removeClass("fake-active");
+  });
+  .bind("touchcancel",
+ function() {
+  var $this = $(this);
+  $this.removeClass("fake-active");
+ });
+ }
